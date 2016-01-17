d6f13cfb72675a791cbfc44d77fc87deb447c821
N11cK <n11ck1706@gmail.com>
2016-01-17 18:46:23 +0100

    edits

--- Assets/Scripts/TrackNoteController.cs
+++ Assets/Scripts/TrackNoteController.cs
@@ -5,6 +5,9 @@ public class TrackNoteController : MonoBehaviour {
 
 
     public float speed;
+	public float hitbarY;
+	public float hitPercent;
+
 	private GameObject ParentTrack;
 	public GameObject parentTrack{
 		get{return ParentTrack;}
@@ -13,19 +16,30 @@ public class TrackNoteController : MonoBehaviour {
 		
 	void Start () {
         gameObject.transform.localScale = new Vector3(TrackManager.trackWidth, 1, 0);
+		hitbarY = -TrackManager.stageDimensions.y*0.70f;
+		//print(hitbarY);
 	}
 
 	void Update () {
         gameObject.transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
 		if (gameObject.transform.position.y <= -TrackManager.stageDimensions.y*1.05) {
+			// didn't click on note
 			Destroy (gameObject);
-			//Debug.Log("Miss!");
 		}
 	}
 
-	// Click on note
+	// Clicked on note
 	void OnMouseDown(){
-		print(" HIT!");
-		Destroy (gameObject);
+		hitPercent = (gameObject.transform.position.y / hitbarY) * 100;
+		if (hitPercent > 100) {hitPercent = 200 - hitPercent;}
+
+		if (gameObject.transform.position.y < hitbarY * 0.5f && gameObject.transform.position.y > hitbarY * 1.5f) {
+			print (" HIT! "+ hitPercent + "%");
+			Destroy (gameObject);
+		} else {
+			print (" MISS! "+ hitPercent + "%");
+			Destroy (gameObject);
+		}
+
 	}
 }
